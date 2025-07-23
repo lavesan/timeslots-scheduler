829c81e4fa0fbbf989780d080d6e9d223ae07b13
Valdery Alves <valdery.jur@gmail.com>
2025-07-23 19:37:23 -0300

    feat: configures graphql
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [PrometheusModule.register()],
+  imports: [
+    PrometheusModule.register(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true, // Automatically generates schema
+      playground: true, // Enables GraphQL playground
+      sortSchema: true, // Sorts schema alphabetically
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
