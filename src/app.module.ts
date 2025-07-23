59f14791ef7dd22770f1aacf4d25e7517f75abb3
Valdery Alves <valdery.jur@gmail.com>
2025-07-23 19:29:07 -0300

    feat: adds and configures apollo
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [PrometheusModule.register()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
